cfb7322aa7953a1576f9336263eb9478e12c3578
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-06-19 11:58:51 +0200

    update

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fff"/>
-        <meta name="msapplication-TileColor" content="#fff"/>
-        <meta name="theme-color" content="#ffffff"></meta> */}
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" async></script>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" async></script>
+        <script async src="https://eocampaign1.com/form/73811a6c-2e1f-11ef-b013-a9db08b09edf.js" data-form="73811a6c-2e1f-11ef-b013-a9db08b09edf"></script>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
